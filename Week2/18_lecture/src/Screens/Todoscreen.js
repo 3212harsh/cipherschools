c36458e5f248e4874e92ff493bcc430d5b817cf8
Harsh choudhary <pyash4048@gmail.com>
2024-06-11 10:38:57 +0530

    Adding first component

---
+++ Week2/18_lecture/src/Screens/Todoscreen.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+
+class Todolist extends Component{
+    render(){
+        return(
+            <>
+                <h1 className='ui heading center'>Welcome to Todolist</h1>
+            </>
+        )
+    }
+}
+
+export default Todolist;
