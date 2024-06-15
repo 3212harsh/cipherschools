105eaf4bbee7824fcc15be2b15367720e3a8ab1e
Harsh choudhary <pyash4048@gmail.com>
2024-06-15 09:12:10 +0530

    Two way binding

---
+++ Week3/24_lecture/src/screens/Todoscreen.js
@@ -0,0 +1,46 @@
+import React,{useState} from 'react'
+import Task from '../components/Task';
+import AddTask from '../components/AddTask';
+
+
+const Todoscreen = () => {
+    const [tasklist,settasklist] = useState([]);
+
+    let addNewTask = (task) => {
+      settasklist([
+        ...tasklist,{...task,createdDate : new Date()}
+      ])
+    }
+
+  return (
+    <>
+    <div>
+      <div className='screen'>
+                <h1 className='ui heading center'>Welcome to Todolist</h1>
+                <div>
+                    <button onClick={(e)=>{
+                        settasklist([
+                            ...tasklist,
+                            {
+                            title:"Going to gym",
+                            description : "Going to gym is good for muscle growth",
+                            createdDate : new Date(),
+                            },
+                        ])
+
+                    }} className='ui secondary button'>Add Task</button>
+                </div><br/>
+
+                  <div className='ui cards'>
+                        {tasklist.map((task,i)=> <Task task={task} key={i}/>)}
+                  </div>
+                  <br/>
+
+                <AddTask onSubmit={addNewTask} />
+            </div>
+    </div>
+    </>
+  )
+}
+
+export default Todoscreen
